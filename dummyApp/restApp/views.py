a16ea3fe36f0fca686670dc8de48f8561708511f
Rahul Shivsharan <Rahul.Shivsharan@ril.com>
2016-01-19 17:44:59 +0530

    Added promise resolving in service

--- dummyApp/restApp/views.py
+++ dummyApp/restApp/views.py
@@ -20,11 +20,19 @@ class ServerView(viewsets.ViewSet):
                 responseData["servers"].append({"name" : "Deployment Server", "os" : "Ubuntu"})
                 responseData["servers"].append({"name" : "Mail Server", "os" : "Windows"})
                 response = Response(responseData)
-                raise "SERVER_CRASH"
-            except "SERVER_CRASH":
-                print e
+                #raise MyExceptions("SERVER_CRASH")
+            except MyExceptions as me:
+                print me
                 response = Response({"msg" : "Fatal Error Occured..."},status=status.HTTP_400_BAD_REQUEST)
             except Exception as e:
                 print e
                 response = Response({"msg" : "Not able to fetch Servers"},status=status.HTTP_400_BAD_REQUEST)
             return response
+
+class MyExceptions(Exception):
+
+	def __init__(self,value):
+		self.value = value
+
+	def __str__(self):
+		return str(self.value)
